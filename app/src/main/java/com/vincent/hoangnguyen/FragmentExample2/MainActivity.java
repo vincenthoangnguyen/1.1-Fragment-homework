2aa41d3c0c99aab76a332107c4732b10b405542a
vincent_ubuntu <hoang.nv.2425@gmail.com>
2022-11-24 19:17:45 +0700

    done 1

--- app/src/main/java/com/vincent/hoangnguyen/FragmentExample2/MainActivity.java
+++ app/src/main/java/com/vincent/hoangnguyen/FragmentExample2/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,31 +14,39 @@ public class MainActivity extends AppCompatActivity {
     private Button mbutton;
     private Boolean isFragmentDisplayed = false;
     static final String STATE_FRAGMENT = "state_of_fragment";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        Log.d("state", isFragmentDisplayed.toString());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState != null) {
-            isFragmentDisplayed =
-                    savedInstanceState.getBoolean(STATE_FRAGMENT);
-            if (isFragmentDisplayed) {
-                // If the fragment is displayed, change button to "close".
+        mbutton = findViewById(R.id.open_button);
+        // saveInstanceState = null nghĩa là activity ko thay đổi trạng thái
+        // ví dụ như cấu hình ko thay đổi, ko chuyển từ dọc sang ngang bla bla
+
+        // nếu != null tức là trạng thái của activity đã thay đổi
+        // lúc này lấy lại trạng thái của fragment và kiểm tra xem nếu nó đang mở thì phải set lại text cho button là đóng
+        // vì sau khi trạng thái thay đổi thì activity đã được khởi tạo lại button lúc fragment đang mở lại thành open
+        if(savedInstanceState != null){
+            isFragmentDisplayed = savedInstanceState.getBoolean(STATE_FRAGMENT);
+            Log.d("state", "get - "+isFragmentDisplayed.toString());
+            if(isFragmentDisplayed){
                 mbutton.setText(R.string.close);
             }
         }
 
 
-        mbutton = findViewById(R.id.open_button);
         mbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(!isFragmentDisplayed){ //false
                     displayFragment();
+                    Log.d("state", "display - "+isFragmentDisplayed.toString());
                 }
-                else
+                else{
                     closeFragment();
-            }
+                    Log.d("state", "close - "+isFragmentDisplayed.toString());
+            }}
         });
     }
     private void displayFragment() {
@@ -60,14 +69,14 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         assert fragmentSimple != null;
         fragmentTransaction.remove(fragmentSimple).commit();
-
         // change button text and boolean
         mbutton.setText(R.string.open);
         isFragmentDisplayed = false;
     }
 
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        savedInstanceState.putBoolean(STATE_FRAGMENT, isFragmentDisplayed);
-        super.onSaveInstanceState(savedInstanceState);
+    public void onSaveInstanceState(Bundle saveState) {
+        saveState.putBoolean(STATE_FRAGMENT, isFragmentDisplayed);
+        Log.d("state", "save - "+isFragmentDisplayed.toString());
+        super.onSaveInstanceState(saveState);
     }
 }
